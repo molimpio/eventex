c9acbf45f611cf12f8eea1df5272ec1855ec53e2
Moises <moiza.olimpio@gmail.com>
2022-10-04 09:10:35 -0300

    Store subscription

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self) -> None:
+        self.obj = Subscription(
+            name='Moises Olimpio',
+            cpf='12345678901',
+            email='moisesolimpiofilho@gmail.com',
+            phone='19-991231234'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attribute"""
+        self.assertIsInstance(self.obj.created_at, datetime)

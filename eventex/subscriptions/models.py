e70bad1cde610fd14bca47c865fe4082ae240281
Moises <moiza.olimpio@gmail.com>
2022-10-11 08:39:59 -0300

    Apply model form

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,13 +1,14 @@
 from uuid import uuid4
 
 from django.db import models
+from eventex.subscriptions.validators import validate_cpf
 
 
 class Subscription(models.Model):
     name = models.CharField('nome', max_length=100)
-    cpf = models.CharField('CPF', max_length=11)
-    email = models.EmailField('e-mail')
-    phone = models.CharField('telefone', max_length=20)
+    cpf = models.CharField('CPF', max_length=11, validators=[validate_cpf])
+    email = models.EmailField('e-mail', blank=True)
+    phone = models.CharField('telefone', max_length=20, blank=True)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
     url_identity = models.UUIDField(default=uuid4())
     paid = models.BooleanField('pago', default=False)

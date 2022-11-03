7f05a99977777d9fe78467d89d879fc0dffa52ec
Moisés Olimpio <molimpio@hotmail.com.br>
2022-11-03 14:22:27 -0300

    Feature Course using mti

--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -39,6 +39,9 @@ class TalkModelTest(TestCase):
     def test_str(self):
         self.assertEqual('Título da Palestra', str(self.talk))
 
+    def test_ordering(self):
+        self.assertListEqual(['start'], Talk._meta.ordering)
+
 
 class PeriodManagerTest(TestCase):
     def setUp(self) -> None:
@@ -82,6 +85,9 @@ class CourseModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual('Título do Curso', str(self.course))
+
+    def test_ordering(self):
+        self.assertListEqual(['start'], Course._meta.ordering)
 
     def test_manager(self):
         self.assertIsInstance(Course.objects, PeriodManager)
